723ef3715e24741853193882ca51403d099c1b0f
Hgnim <hgn1m@outlook.com>
2026-04-16 22:17:37 +0800

    在游戏菜单中可通过键盘按键开始游戏

--- scenes/menu/Menu.cs
+++ scenes/menu/Menu.cs
@@ -1,26 +1,35 @@
 using Godot;
 using System;
 
-public class Menu : Control
-{
+public class Menu : Control {
     Label about;
-    public override void _Ready()
-    {
+    public override void _Ready() {
         about = GetNode<Label>("about");
 
         LoadAbout();
-	}
+    }
 
     void LoadAbout() {
         about.Text =
             "版本：" + About.version + "\n" + About.copyright;
-	}
+    }
 
 #pragma warning disable IDE0051
-	private void On_startGameBtn_pressed() {
+    private void On_startGameBtn_pressed() {
 #pragma warning restore IDE0051
-        DataCore.Instance.gameData.GameState = GameData.GameStateEnum.loading;
+        if (
+            DataCore.Instance.gameData.GameState != GameData.GameStateEnum.loading
+			&& DataCore.Instance.gameData.GameState != GameData.GameStateEnum.running
+           ) {
+            DataCore.Instance.gameData.GameState = GameData.GameStateEnum.loading;
 
-		GetTree().ChangeScene("res://scenes/main/main.tscn");
-	}
+            GetTree().ChangeScene("res://scenes/main/main.tscn");
+        }
+    }
+
+    public override void _Input(InputEvent ie) {
+        if (ie.IsActionPressed("key_enter") || ie.IsActionPressed("key_space")) {
+            On_startGameBtn_pressed();
+		}
+    }
 }
--- scenes/menu/gameOver/GameOver.cs
+++ scenes/menu/gameOver/GameOver.cs
@@ -11,10 +11,21 @@ public class GameOver : Control
 #pragma warning disable IDE0051
 	private void On_restartGameBtn_pressed() {
 #pragma warning restore IDE0051
-		DataCore.Instance.InitData();
+		if (
+			DataCore.Instance.gameData.GameState != GameData.GameStateEnum.loading
+			&& DataCore.Instance.gameData.GameState != GameData.GameStateEnum.running
+		   ) {
+			DataCore.Instance.InitData();
 
-		DataCore.Instance.gameData.GameState = GameData.GameStateEnum.loading;
+			DataCore.Instance.gameData.GameState = GameData.GameStateEnum.loading;
 
-		GetTree().ChangeScene("res://scenes/main/main.tscn");
+			GetTree().ChangeScene("res://scenes/main/main.tscn");
+		}
+	}
+
+	public override void _Input(InputEvent ie) {
+		if (ie.IsActionPressed("key_enter")) {
+			On_restartGameBtn_pressed();
+		}
 	}
 }
