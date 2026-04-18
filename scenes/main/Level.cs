700219b1d155d7cd66f9686f054ea9be2a07ffc6
Hgnim <hgn1m@outlook.com>
2026-04-18 15:49:15 +0800

    分数变化来源文字条目将不与游戏中的主要内容碰撞；关卡层级调整

--- scenes/main/Level.cs
+++ scenes/main/Level.cs
@@ -38,6 +38,7 @@ public class Level : Timer
         switch (level) {
             case 0:
 				enemySpawns[0].WaitTime = 4;
+				enemySpawn0_objBasicSpeed = 100;
 				enemySpawns[0].Start();
 				break;
 			case 1:
@@ -45,31 +46,48 @@ public class Level : Timer
 				break;
 			case 2:
 				enemySpawns[1].WaitTime = 5;
+				enemySpawn1_objBasicSpeed = 150;
 				enemySpawns[1].Start();
 				break;
 			case 3:
-				enemySpawns[0].WaitTime = 1f;
+				enemySpawn0_objBasicSpeed = 200;
 				break;
 			case 4:
 				enemySpawns[1].WaitTime = 3;
 				break;
 			case 5:
-				enemySpawns[1].WaitTime = 1.5f;
+				enemySpawn1_objBasicSpeed = 250;
+				break;
+			case 6:
+				enemySpawns[0].WaitTime = 1;
+				break;
+			case 7:
+				enemySpawns[1].WaitTime = 2;
+				break;
+			case 8:
+				enemySpawn0_objBasicSpeed = 300;
+				break;
+			case 9:
+				enemySpawn1_objBasicSpeed = 350;
 				break;
 		}
 	}
 
+	float enemySpawn0_objBasicSpeed = 100;
 #pragma warning disable IDE0051
 	private void On_enemySpawn0_timeout() {
 #pragma warning restore IDE0051
-		var fcObj = (RigidBody2D)FireChargeObject.Instance();
+		FireCharge fcObj = (FireCharge)FireChargeObject.Instance();
+		fcObj.BasicSpeed = enemySpawn0_objBasicSpeed;
 		GetTree().CurrentScene.AddChild(fcObj);
 		fcObj.GlobalPosition = enemySpawns_pos[0].GlobalPosition;
 	}
+	float enemySpawn1_objBasicSpeed = 100;
 #pragma warning disable IDE0051
 	private void On_enemySpawn1_timeout() {
 #pragma warning restore IDE0051
-		var fcObj = (RigidBody2D)FireChargeObject.Instance();
+		FireCharge fcObj = (FireCharge)FireChargeObject.Instance();
+		fcObj.BasicSpeed = enemySpawn1_objBasicSpeed;
 		GetTree().CurrentScene.AddChild(fcObj);
 		fcObj.GlobalPosition = enemySpawns_pos[1].GlobalPosition;
 	}
